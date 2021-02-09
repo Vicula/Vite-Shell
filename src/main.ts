a303733e87627a53e2b4e7aaf7d3410c706492a2
Victor Carpenter <victorcarpenter123@gmail.com>
2021-02-09 13:14:21 -0500

    base app

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
+/**
+ * Cherry Blossoms Effect Init Script
+ * ------------------------------------------------------------------------------
+ * A file that inits a vue shell and adds animation compoenents for
+ * site building
+ *
+ * current features:
+ *  - NONE
+ *
+ * @namespace cherryBlossoms
+ */
+
 import { createApp } from 'vue'
 
-createApp({}).mount('#app')
+createApp({}).mount('body')
