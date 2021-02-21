71b135a29c743079c79282f9731ccab79b5db582
Victor Carpenter <victorcarpenter123@gmail.com>
2021-02-21 18:44:05 -0500

    clean printing

--- plugins/vite-plugin-shopify-theme/index.ts
+++ plugins/vite-plugin-shopify-theme/index.ts
@@ -4,17 +4,32 @@ import { resolve } from 'path'
 import fs from 'fs'
 import chalk from 'chalk'
 import { shopifyTheme } from './types'
+import { ViteDevServer, UserConfig, ResolvedConfig } from 'vite'
 
 export default function execute(i) {
   return new ShopifyPlugin(i)
 }
 
-// take in variable names
+// take in :
+// variable names for shopify pass/store
+// directory location for shopify files
+// config location
 
 class ShopifyPlugin {
   name: string
   env: { [k: string]: string }
   buildStart: () => Promise<void>
+  private server: ViteDevServer
+  private resolvedConfig: ResolvedConfig
+  configResolved(rc: ResolvedConfig) {
+    this.resolvedConfig = rc
+  }
+  configureServer(_s: ViteDevServer) {
+    this.server = _s
+  }
+  config(c: UserConfig) {
+    this.checkHttps(c)
+  }
 
   constructor(i) {
     this.name = 'shopify-plugin'
@@ -25,6 +40,27 @@ class ShopifyPlugin {
 
   private configureServerMiddleware() {
     // need to inject middleware for making requests to shopify
+  }
+
+  private print(t: string, m: string): void {
+    console.log(`${chalk.blue.bold(`Shopify-Plugin:${t}`)} ${m}`)
+    console.log('____________________')
+    console.log('')
+  }
+  private printError(t: string, m: string): void {
+    console.error(
+      `${chalk.whiteBright.bgRed.bold(
+        `Shopify-Plugin:${t}`
+      )} ${chalk.underline.red(m)}`
+    )
+    console.error('____________________')
+    console.log('')
+  }
+
+  private checkHttps(c: UserConfig) {
+    !c.server.https &&
+      this.print('Config', 'Https wasnt turned on; Doing so now ...')
+    !c.server.https && (c.server.https = true)
   }
 
   private async setEnv() {
@@ -83,57 +119,39 @@ class ShopifyPlugin {
     })
   private themeCreate(n: string) {
     !fs.existsSync('.build') && fs.mkdirSync('.build')
-    console.log(
-      `${chalk.white.bold('Shopify-Plugin:Create')} ${chalk.white(
-        'Couldnt find a theme related to this git branch; so creating one with name'
-      )} ${chalk.white.underline.bold(n)} ...`
+    this.print(
+      'Create',
+      `Couldnt find a theme related to this git branch; so creating one with name ${chalk.underline(
+        n
+      )} ...`
     )
-    console.log('____________________')
-    console.log('')
     execSync(
       `theme new -p=${this.env.SHOPIFY_PASSWORD} -s=${this.env.SHOPIFY_STORE} -n=${n} -d=.build`
     )
-    console.log(
-      `${chalk.white.bold('Shopify-Plugin:Create')} ${chalk.white(
-        'Theme created called'
-      )} ${chalk.white.underline.bold(n)} ; ${chalk.white(
-        'Cleaning up and deploying theme ...'
-      )}`
+    this.print(
+      'Create',
+      `Theme created called ${chalk.underline(
+        n
+      )}; Cleaning up and deploying theme ...`
     )
-    console.log('____________________')
-    console.log('')
     fs.existsSync('.build') && rimraf.sync('.build')
     execSync(`theme deploy -n -d=src/shopify`)
-    console.log(
-      `${chalk.white.bold('Shopify-Plugin:Create')} ${chalk.white(
-        '✨✨ Theme Deployed and cleaned ✨✨'
-      )}`
-    )
-    console.log('____________________')
-    console.log('')
+    this.print('Create', `✨✨ Theme Deployed and cleaned ✨✨`)
   }
   private themeFetch(i: string) {
-    console.log(
-      `${chalk.white.bold('Shopify-Plugin:Create')} ${chalk.white(
-        'Found theme on shopify; fetching theme files ...'
-      )}`
-    )
-    console.log('____________________')
-    console.log('')
+    this.print('Fetch', `Found theme on shopify; fetching theme files ...`)
     execSync(
       `theme get -p=${this.env.SHOPIFY_PASSWORD} -s=${this.env.SHOPIFY_STORE} -t=${i} -d=src/shopify --ignored-file=assets/* --ignored-file=locales/* --ignored-file=config/*`
     )
-    console.log(
-      `${chalk.white.bold('Shopify-Plugin:Create')} ${chalk.white(
-        '✨✨ Theme Fetched ✨✨'
-      )}`
-    )
-    console.log('____________________')
-    console.log('')
+    this.print('Fetch', '✨✨ Theme Fetched ✨✨')
   }
 
   private async createConfig(r: () => void, x: (k: Error) => void) {
     try {
+      this.print(
+        'Config',
+        '🔧 Checking shopify themes and comparing them to git repo 🔧'
+      )
       const t = await this.getThemes()
       const b = await this.getBranch()
       if (b !== 'main' && b !== 'master') {
@@ -147,93 +165,31 @@ class ShopifyPlugin {
             throw 'Shopify-Plugin:Error Cannot edit a branch direcetly connected to a live theme'
           }
         } catch (ex) {
-          console.error(
-            `${chalk.white.bgRed.bold(
-              'Shopify-Plugin:Error'
-            )} ${chalk.underline.red(
-              'Cannot edit a branch direcetly connected to a live theme'
-            )}`
+          this.printError(
+            'Error',
+            `Cannot edit a branch direcetly connected to a live theme'`
           )
-          console.log('____________________')
-          console.log('')
           x(new Error(ex))
         }
       } else {
         throw 'Shopify-Plugin:Error Cannot build on git branch (master|main)'
       }
     } catch (er) {
-      console.error(
-        `${chalk.white.bgRed.bold(
-          'Shopify-Plugin:Error'
-        )} ${chalk.underline.red('Cannot build on git branch (master|main)')}`
-      )
-      console.log('____________________')
-      console.log('')
+      this.printError('Error', 'Cannot build on git branch (master|main)')
       x(new Error(er))
     }
-
-    // if main or master theme get --live
-    // specify -d / --dir for directory of shopify templates
-
-    // fs.writeFile('config.yml', `config yaml `, (err) => {
-    //   if (err) console.log(err)
-    // })
   }
+
   private createJSONFiles() {
     fs.writeFile('layout.json.liquid', `{{products | json}} `, (err) => {
       if (err) console.log(err)
     })
   }
 
-  private checkFolders() {
-    if (!fs.existsSync('shopify')) {
-      fs.mkdirSync('shopify')
-    }
-  }
   private createBuildStartHook() {
     this.buildStart = () =>
       new Promise((resolve, reject) => {
         this.createConfig(resolve, reject)
       })
   }
-  private runScript(c: string) {
-    exec(c)
-  }
 }
-
-// BUILD START on dev env
-
-// create config file from env file
-
-// git branch --show-current
-// theme get --list
-
-// check if branch is in the theme list
-// if not then do
-
-// theme new --password=${process.env.SHOPIFY_PASSWORD} --store=${process.env.SHOPIFY_STORE} --name=${gitBranch}
-// theme deploy
-// theme watach
-// theme open
-
-// if theme does exist then
-
-// theme configure --password=${process.env.SHOPIFY_PASSWORD} --store=${process.env.SHOPIFY_STORE} --themeid=${themeID}
-//theme watch
-// theme open
-
-// check for shopify folders like below
-// var fs = require('fs');
-// var dir = './tmp';
-
-// if (!fs.existsSync(dir)){
-//     fs.mkdirSync(dir);
-// }
-
-// if not then we need to run
-// theme get
-
-// TRANSFORM HTML FROM SHOPIFY
-
-// BUILD END
-// we will just run a simple theme deploy from dist
